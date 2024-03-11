879ec6d02fd972538387078e491472862c79421d
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-03-11 17:38:51 -0600

    Login y Register completos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,17 @@
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
